7c4fcfc99950dae6f8d95e464a01c25fb3422ae6
LexxieR <lexxie_roy@hotmail.com>
2025-02-22 19:08:06 -0500

    Updated the menu navigation so that on hover the buttons change colour and fixed a bug with the dropdown menu.

--- header.js
+++ header.js
@@ -2,18 +2,22 @@ window.onload = function () {
     document.getElementById("header").innerHTML = `
         <header>
             <nav class="menu-container">
-    <ul class="menu-list">
-        <li><a href="index.html">Home</a></li>
-        <li><a href="about.html">About me</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="contact.html">Contact</a></li>
-    </ul>
-    <ul class="lan-menu">
-        <li><a href="#" onclick="setLanguage('en')">English</a></li>
-        <li><a href="#" onclick="setLanguage('fr')">Français</a></li>
-    </ul>
-</nav>
-
+                <ul class="menu-list">
+                    <li class="menu-item"><a href="index.html">Home</a></li>
+                    <li class="menu-item"><a href="about.html">About me</a></li>
+                    <li class="menu-item"><a href="#">Projects</a>
+                        <ul class="dropdown">
+                            <li><a href="#">Project 1!!!!!!!!!!!!!!!</a></li>
+                            <li><a href="#">Project 2</a></li>
+                        </ul>
+                    </li>    
+                    <li class="menu-item"><a href="contact.html">Contact</a></li>
+                </ul>
+                <ul class="lan-menu">
+                    <li class="menu-item"><a href="#" onclick="setLanguage('en')">English</a></li>
+                    <li class="menu-item" ><a href="#" onclick="setLanguage('fr')">Français</a></li>
+                </ul>
+            </nav>
         </header>
     `;
 };
